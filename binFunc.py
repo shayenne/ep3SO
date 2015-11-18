3cd9c9f0402d1d96f97bdf5f4caeb1c20cae623d
Shayenne Luz Moura <shayenne.moura@usp.br>
2015-11-18 16:02:40 -0200

    Funcoes iniciais

--- binFunc.py
+++ binFunc.py
@@ -1,24 +1,42 @@
+from datetime import datetime
+import struct
+import mmap
+import os
+
+
+def makeEmptyBin(filename, size):
+    with open(filename, 'wb') as f:
+        for i in xrange(size):
+            f.seek(i)
+            f.write(b'\x00')
+        f.close()
+
 def escreveDados(arquivo, dados, posicao):
-    with open(arquivo, 'wb') as f:
+    with open(arquivo, 'ab') as f:
         f.seek(posicao)
         f.write(dados)
 
-        
+"""        
 def copiaArquivo(origem, destino, posicao):
     with open(origem,'rb') as f1:
         with open(destino,'wb') as f2:
             # escrever no sistema de arquivos a partir de uma posicao
             f2.seek(posicao)#f2 sera o nosso sistema de arquivos
-            # posicao eh a posicao em que comecam os dados do arquivo
+            # posicao eh a posicao (em bytes) em que comecam os dados do arquivo
             while True:
-                bytes=f1.read(1024)
+                # 4kB - 4B
+                bytes=f1.read(3996)
                 if bytes: 
                     for byte in bytes:
                         pass    # process the bytes if this is what you want
-
+                    # Coloca o endereco do primeiro bloco livre encontrado
+                    # Seta o contador com -1
+                    n = f2.write("22")
+                    # Coloca o resto do conteudo do arquivo no bloco
                     n=f2.write(bytes)
                 else:
                     break
+"""
 
 def leDados(arquivo, inicio, fim):
     with open(arquivo, 'rb') as f:
@@ -26,11 +44,104 @@ def leDados(arquivo, inicio, fim):
         data = f.read(fim-inicio)
     return data
 
+# Pos no intervalo [0:25000], refere-se aos bits do bitmap
+# Troca o valor do bitmap da posicao para seu complementar
+def switchBitmap(arqmem, pos):
+    # Acessa uma posicao especifica do arquivo de memoria
+    mapmem = memory_map(arqmem)
+
+    val = pos/8
+    rest = pos % 8
+
+    p = ord(mapmem[val+4000])
+
+    # Seta o bit da posicao "pos" para o valor "bit" 
+    p^= 1 << rest
+    mapmem[val+4000] = chr(p)
+
+    mapmem.close()
+
+def getBitmap(arqmem, pos):
+    mapmem = memory_map(arqmem)
+    val = pos/8
+    rest = pos % 8
+
+    p = ord(mapmem[val+4000])
+
+    # Seta o bit da posicao "pos" para o valor "bit" 
+    p&= 1 << rest
+    mapmem.close()
+    
+    return p
+    
+    
+
+
+def memory_map(filename, access=mmap.ACCESS_WRITE):
+    size = os.path.getsize(filename)
+    fd = os.open(filename, os.O_RDWR)
+    return mmap.mmap(fd, size, access=access)
+
+def escreveIntBin(arqmem, ini, pid):
+    # Acessa uma posicao especifica do arquivo de memoria
+    mapmem = memory_map(arqmem)
+
+    data = struct.pack("h", pid)
+    mapmem[ini:ini+2] = data
+
+    mapmem.close()
+
+def leIntBin(arqmem, pos):
+    # Acessa uma posicao especifica do arquivo de memoria
+    mapmem = memory_map(arqmem)
+    data = mapmem[pos:pos+2]
+    data = struct.unpack("h", data)
+    mapmem.close()
+    return int(data[0])
+
+def escreveIntervalo(arqmem, ini, fim, conteudo):
+    # Acessa uma posicao especifica do arquivo de memoria
+    mapmem = memory_map(arqmem)
+
+    print len(conteudo), fim-ini
+    mapmem[ini:ini+len(conteudo)] = conteudo
+
+    mapmem.close()
+                    
+
+
+def leIntervalo(arqmem, ini, fim):
+    # Acessa uma posicao especifica do arquivo de memoria
+    mapmem = memory_map(arqmem)
+    data = mapmem[ini : fim]
+        
+    #print mapmem[pos]
+    mapmem.close()
+    return data
 
 if __name__=="__main__":
     escreveDados("test", "ABCDEFGHSHAYENNE", 0)
+
+    escreveDados("test", "ESCREVI", 30)
+
+
+    escreveIntBin("primeiro", 0, 1)
+    escreveIntBin("primeiro", 2, 2)
+    escreveIntBin("primeiro", 4, 3)
+    escreveIntBin("primeiro", 6, 25000)
+
+    bitmap = leIntBin("primeiro", 0)
+    raiz = leIntBin("primeiro", 2)
+    dirs = leIntBin("primeiro", 6)
+
+    print bitmap, raiz, dirs
+
+    escreveDados("data", bin(100000000), 0)
 
     copiaArquivo("test", "me", 10)
 
     # le dados de um arquivo [inicio, fim)
     print leDados("me", 10, 18)
+
+
+    copiaArquivo("test", "primeiro", 4000)
--- sistema.py
+++ sistema.py
@@ -1,9 +1,93 @@
+from binFunc import *
+
 class SistemaArquivos:
 
+    nome = None
     raiz = None
-    diretorios = []
-    espaco = None
+    dirs = []
+    bitmap = None
     FAT = []
+    tam = 4000
 
     def __init__(self, nome):
         # se o sistema de arquivos nao existe, criar um novo
+        self.nome = nome
+        try:
+            with open(self.nome, 'rb') as fileSystem:
+                print "Sistema existe"
+                self.bitmap = leIntBin(self.nome, 0)
+                self.raiz = leIntBin(self.nome, 2)
+                self.dirs = leIntBin(self.nome, 4)
+
+                print self.bitmap, self.raiz, self.dirs
+                
+        except IOError:
+            self.criaSistemaArquivos()
+
+    def criaSistemaArquivos(self):
+        fileSystem = self.nome
+        # Endereco de blocos
+        self.bitmap = 1
+        self.raiz = 2
+        self.dirs = 3
+
+        makeEmptyBin(fileSystem, 28000)
+
+        escreveIntBin(fileSystem, 0, self.bitmap)
+        switchBitmap(fileSystem, 0)
+        switchBitmap(fileSystem, 1)
+        switchBitmap(fileSystem, 2)
+        switchBitmap(fileSystem, 3)
+        switchBitmap(fileSystem, 4)
+        switchBitmap(fileSystem, 5)
+        switchBitmap(fileSystem, 6)
+        #switchBitmap(fileSystem, 3)
+        if not getBitmap(fileSystem, 8):
+            print "Encontrei espaco vazio"
+        else:
+            print "Sai daqui"
+        escreveIntBin(fileSystem, 2, self.raiz)
+        escreveIntBin(fileSystem, 4, self.dirs)
+
+    # Le o bloco de numero "bloco" e retorna o proximo apontado pelo bloco
+    # lido
+    def leBloco(self, bloco):
+        ini = bloco*self.tam
+        data = leIntervalo(self.nome, ini, ini+self.tam)
+        # Ponteiro para o proximo bloco
+        prox = int(struct.unpack("h", data[0:2])[0])
+        print prox
+        # Contador de conteudo
+        cont = int(struct.unpack("h", data[2:4])[0])
+        print cont
+
+        return prox, data[4:]
+
+    def leArquivo(self, endereco):
+        prox, conteudo = self.leBloco(endereco)
+        while prox != -1:
+            print conteudo
+            prox, conteudo = self.leBloco(prox)
+        print conteudo
+
+
+    # OBS: Nunca recebe um conteudo maior que 4 kB
+    def escreveBloco(self, endereco, conteudo):
+        ini = endereco*self.tam
+        escreveIntervalo(self.nome, ini, ini+self.tam, conteudo)
+
+    # Retorna o primeiro endereco de bloco vazio encontrado
+    def FirstFit(self):
+        for i in xrange(25000):
+            if not getBitmap(self.nome, i):
+                return i
+        return "ACABOU O ESPACO"
+
+if __name__=="__main__":
+    teste = SistemaArquivos("primeiro")
+    teste.escreveBloco(3, struct.pack("h", 4)+struct.pack("h", 0)+"aiudhaisudiausdiaushdiuashd")
+    teste.escreveBloco(4, struct.pack("h", -1)+struct.pack("h", 0)+ "zczzijisdfj")
+    teste.leArquivo(3)
+    print "Encontrei o espaco ", teste.FirstFit()
+
+    
